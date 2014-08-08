3d355a5552f07ce32893eb3d3fcade8f17e60f2d
hsestupin <stupin.sergey@gmail.com>
2014-08-08 10:03:08 +0400

    steal more tests from https://raw.githubusercontent.com/domchristie/to-markdown/master/test/tests.js

--- src/html_to_md/core.clj
+++ src/html_to_md/core.clj
@@ -1,5 +1,5 @@
 (ns html-to-md.core
-  (:import (java.io StringReader BufferedWriter StringWriter))
+  (:import (java.io StringReader StringWriter Writer))
   (:require [net.cgrand.enlive-html :as html]
             [clojure.string :as str]
             [clojure.java.io :as io]))
@@ -9,20 +9,20 @@
 
 (declare html-to-md*)
 
-(defn new-line [^BufferedWriter out {:keys [blockquote-nested-level]}]
+(defn new-line [^Writer out {:keys [blockquote-nested-level]}]
   "Every time you need to insert new line into output this function must be called.
   It takes into account :blockquote tags and inserts '> ' if needed."
   (doto out
-    (.newLine)
+    (.write "\n")
     (.write (apply str (repeat blockquote-nested-level "> ")))))
 
-(defmethod convert :body [{:keys [content]} ^BufferedWriter out state]
+(defmethod convert :body [{:keys [content]} ^Writer out state]
   (html-to-md* out content state))
 
-(defmethod convert :html [{:keys [content]} ^BufferedWriter out state]
+(defmethod convert :html [{:keys [content]} ^Writer out state]
   (html-to-md* out content state))
 
-(defmethod convert :p [{:keys [content]} ^BufferedWriter out state]
+(defmethod convert :p [{:keys [content]} ^Writer out state]
   (when content
     (doto out
       (new-line state)
@@ -30,7 +30,7 @@
       (html-to-md* content state)
       (new-line state))))
 
-(defmethod convert :br [_ ^BufferedWriter out state]
+(defmethod convert :br [_ ^Writer out state]
   (new-line out state))
 
 (defn write-h [content out h-level state]
@@ -44,13 +44,13 @@
 
 (defmacro defheading [h-level]
   `(defmethod convert (keyword (str "h" ~h-level))
-              [el# ^BufferedWriter out# state#]
+              [el# ^Writer out# state#]
      (write-h (:content el#) out# ~h-level state#)))
 
 (dotimes [i 6]
   (defheading (inc i)))
 
-(defmethod convert :hr [_ ^BufferedWriter out state]
+(defmethod convert :hr [_ ^Writer out state]
   (doto out
     (new-line state)
     (new-line state)
@@ -60,7 +60,7 @@
 (defn quoted-title [title]
   (if title (str " \"" title \") ""))
 
-(defmethod convert :a [{{:keys [href title]} :attrs content :content} ^BufferedWriter out state]
+(defmethod convert :a [{{:keys [href title]} :attrs content :content} ^Writer out state]
   (if href
     (doto out
       (.append \[)
@@ -78,11 +78,11 @@
       (html-to-md* content state)
       (.write "**"))))
 
-(defmethod convert :b [{:keys [content]} ^BufferedWriter out _]
-  (write-b-or-strong content out))
+(defmethod convert :b [{:keys [content]} ^Writer out state]
+  (write-b-or-strong content out state))
 
-(defmethod convert :strong [{:keys [content]} ^BufferedWriter out _]
-  (write-b-or-strong content out))
+(defmethod convert :strong [{:keys [content]} ^Writer out state]
+  (write-b-or-strong content out state))
 
 (defn write-i-or-em [content out state]
   (when content
@@ -91,13 +91,20 @@
       (html-to-md* content state)
       (.append \_))))
 
-(defmethod convert :i [{:keys [content]} ^BufferedWriter out _]
-  (write-i-or-em content out))
+(defmethod convert :i [{:keys [content]} ^Writer out state]
+  (write-i-or-em content out state))
 
-(defmethod convert :em [{:keys [content]} ^BufferedWriter out _]
-  (write-i-or-em content out))
+(defmethod convert :em [{:keys [content]} ^Writer out state]
+  (write-i-or-em content out state))
 
-(defmethod convert :img [{{:keys [src alt title]} :attrs} ^BufferedWriter out _]
+(defmethod convert :code [{:keys [content]} ^Writer out state]
+  (when content
+    (doto out
+      (.append \`)
+      (html-to-md* content state)
+      (.append \`))))
+
+(defmethod convert :img [{{:keys [src alt title]} :attrs} ^Writer out _]
   (doto out
     (.write "![")
     (.write (if alt alt ""))
@@ -106,11 +113,21 @@
     (.write (quoted-title title))
     (.append \))))
 
-(defmethod convert :pre [{:keys [content]} ^BufferedWriter out state]
-  (let [indent "    "
-        code-block (-> (first content)
-                       (str/replace #"\t+/g" "  ")          ; convert tabs to spaces
-                       (str/replace #"\n" (str "\n" indent)))] ; add indents
+(defn trim-code [code]
+  (let [length (.length code)]
+    (if (and
+          (.endsWith code "`")
+          (.startsWith code "`")
+          (> length 1))
+      (.substring code 1 (dec length)))))
+
+(defmethod convert :pre [{:keys [content]} ^Writer out state]
+  (let [indent (apply str (repeat 4 \space))
+        code-block (-> (html-to-md* content state)
+                       (trim-code)
+                       (str/replace #"(?m)^\t+" "  ")       ; convert tabs to spaces
+                       (str/replace #"\n" (str "\n" indent))
+                       )]                                   ; add indents
     (doto out
       (new-line state)
       (new-line state)
@@ -125,21 +142,22 @@
         (recur (conj indexed-coll (assoc-in head [:attrs :li-index] li-index)) tail (inc li-index))
         (recur (conj indexed-coll (if (string? head) (str/trim head) head)) tail li-index)))))
 
-(defn convert-list [list-type content out list-nested-level state]
-  (let [new-nested-level (inc list-nested-level)
-        indexed-content (with-li-index content)]
-    ;(println "indexed-content: " indexed-content)
-    (html-to-md* out
-                 indexed-content
-                 (-> state
-                     (assoc :list-type list-type)           ; specify current list-type
-                     (assoc :list-nested-level new-nested-level)))))
+(defn convert-list [list-type content out state]
+  (let [indexed-content (with-li-index content)]
+    (let [list (-> (html-to-md* indexed-content
+                                (-> state
+                                    (assoc :list-type list-type))) ; specify current list-type
+                   (str/replace #"([ \t]*\n|\s+)$" ""))]
+      (doto out
+        (new-line state)
+        (new-line state)
+        (.write list)))))
 
-(defmethod convert :ol [{:keys [content]} ^BufferedWriter out {:keys [list-nested-level] :as state}]
-  (convert-list :ol content out list-nested-level state))
+(defmethod convert :ol [{:keys [content]} ^Writer out state]
+  (convert-list :ol content out state))
 
-(defmethod convert :ul [{:keys [content]} ^BufferedWriter out {:keys [list-nested-level] :as state}]
-  (convert-list :ul content out list-nested-level state))
+(defmethod convert :ul [{:keys [content]} ^Writer out state]
+  (convert-list :ul content out state))
 
 (defn ol-prefix [li-index]
   (cond
@@ -147,39 +165,61 @@
     (< li-index 100) (str li-index ". ")
     :else (str li-index ".")))
 
-(defn trim-all-strings [coll]
-  (map #(if (string? %) (str/trim %) %) coll))
-
 (defmethod convert :li
-           [{{:keys [li-index]} :attrs content :content} ^BufferedWriter out {:keys [list-nested-level list-type] :as state}]
-  (doto out
-    (new-line state)
-    (.write (apply str (repeat (dec list-nested-level) "    ")))
-    (.write (if (= :ol list-type)
-              (ol-prefix li-index)
-              "*   "))
-    (html-to-md* (trim-all-strings content) state)))
+           [{{:keys [li-index]} :attrs content :content} ^Writer out {:keys [list-type] :as state}]
+  (let [indent (apply str (repeat 4 \space))
+        li-block (-> (html-to-md* content state)
+                     (str/replace #"\n" (str "\n" indent))  ;prefix this <li> content with indent
+                     (str/replace #"^\s+" ""))]             ;trim beginning whitespaces for correct list rendering
+    ;(println "li-block:" li-block)
+    (doto out
+      (.write (if (= :ol list-type)
+                (ol-prefix li-index)
+                "*   "))
+      (.write li-block)
+      (new-line state))))
 
-(defmethod convert :blockquote [{:keys [content]} ^BufferedWriter out state]
+(defmethod convert :blockquote [{:keys [content]} ^Writer out state]
   (html-to-md* out content (update-in state [:blockquote-nested-level] inc)))
 
-(defn html-to-md* [out html state]
-  (doall (map #(if (map? %)
-                (convert % out state)
-                (.write out %)) html)))
+(defmethod convert :default [{:keys [tag content]} ^Writer out state]
+  (doto out
+    (.write (str "<" tag ">"))
+    (html-to-md* content (update-in state [:blockquote-nested-level] inc))
+    (.write (str "</" tag ">"))))
+
+(defn html-to-md*
+  ([html state]
+   (with-open [^Writer out (StringWriter.)]
+     (html-to-md* out html state)
+     (.flush out)
+     (.toString out)))
+  ([out html state]
+   (doall (map #(if (map? %)
+                 (convert % out state)
+                 (do
+                   (println "WRITING STRING:'" % "'") (.write out %))) html))))
 
 (defn html-to-md
   "Parses input html stream and writes result to out stream"
   [in out]
-  (with-open [^BufferedWriter writer (io/writer out)]
+  (with-open [writer (io/writer out)]
     (html-to-md* writer (html/html-resource in)
-                 {:list-nested-level 0
-                  :blockquote-nested-level 0})
-    (.flush writer)))
+                 {:blockquote-nested-level 0})
+    (.flush writer)
+    out))
+
+(defn clean-up [^String result]
+  (-> result
+      (str/replace #"^[\t\r\n]+|[\t\r\n ]+$" "")            ; trim leading/trailing whitespace. Preserve spaces at the beginning
+      (str/replace #"\n\s+\n" "\n\n")
+      (str/replace #"\n{3,}" "\n\n")))                      ;limit consecutive linebreaks to 2
 
 (defn html-to-md-string
   "Taking html string as input parses it to md string"
   [html-str]
   (with-open [output (StringWriter.)]
-    (html-to-md (StringReader. html-str) output)
-    (.toString output)))
+    (->
+      (html-to-md (StringReader. html-str) output)
+      (.toString)
+      (clean-up))))
--- test/html_to_md/core_test.clj
+++ test/html_to_md/core_test.clj
@@ -1,13 +1,162 @@
 (ns html-to-md.core-test
+  (:use [clojure.template])
   (:require [clojure.test :refer :all]
-            [html-to-md.core :refer :all]))
+            [html-to-md.core :refer :all]
+            [clojure.string :as str]))
 
-(deftest simple-convert
+(defmacro check [input expected msg]
+  `(is (= (html-to-md-string ~input) ~expected) ~msg))
 
+(deftest converts
   (testing "converting p elements"
-    (is (= (html-to-md-string "<p>Lorem ipsum</p>") "Lorem ipsum") "We expect p tags to be wrapped with two line breaks"))
+    (check "<p>Lorem ipsum</p>" "Lorem ipsum" "We expect p tags to be wrapped with two line breaks")
+    (check "<p class='intro'>Lorem ipsum</p>" "Lorem ipsum" "We expect p tags to be wrapped with two line breaks"))
 
-  (testing "Simple convert html to md"
-    (let [line-sep (System/lineSeparator)]
-      (is (= (str line-sep line-sep "### Some HTML" line-sep)
-             (html-to-md-string "<h3>Some HTML</h3>"))))))
+  (testing "converting emphasis elements"
+    (check "<b>Hello world</b>" "**Hello world**" "We expect <b>Hello world</b> to be converted to **Hello world**")
+    (check "<strong>Hello world</strong>"
+           "**Hello world**"
+           "We expect <strong>Hello world</strong> to be converted to **Hello world**")
+    (check "<b></b>" "" "We expect b tags to be removed")
+    (check "<i>Hello world</i>" "_Hello world_" "We expect <i>Hello world</i> to be converted to _Hello world_")
+    (check "<em>Hello world</em>" "_Hello world_" "We expect <em>Hello world</em> to be converted to _Hello world_")
+    (check "<em id='one' class='cowabunga'>Hello world</em>"
+           "_Hello world_"
+           "We expect <em id='one' class='cowabunga'>Hello world</em> to be converted to _Hello world_")
+    (check "<em id='one' class='cowabunga'></em>" "" "We expect empty em tags to be removed"))
+
+  (testing "converting inline code elements"
+    (check "<code>print()</code>" "`print()`" "We expect inline code tags to be converted to backticks")
+    (check "<code></code>" "" "We expect empty code tags to be removed")
+    (check "<code>&lt;video&gt;</code>" "`<video>`" "We expect HTML character references to be decoded")
+    (check "<code>foo&#x1D306;bar</code>" "`foo\uD834\uDF06bar`" "We expect HTML character references to be decoded"))
+
+  (testing "converting hr elements"
+    (check "<hr />" "* * *" "We expect hr elements to be converted to * * *")
+    (check "<hr/>" "* * *" "We expect hr elements to be converted to * * *")
+    (check "<hr>" "* * *" "We expect hr elements to be converted to * * *")
+    (check "<hr class='fancy' />" "* * *" "We expect hr elements to be converted to * * *"))
+
+  (testing "converting br elements"
+    (check "Hello<br />world" "Hello\nworld" "We expect br elements to be converted to valid line separator")
+    (check "Hello<br/>world" "Hello\nworld" "We expect br elements to be converted to valid line separator")
+    (check "Hello<br>world" "Hello\nworld" "We expect br elements to be converted to valid line separator"))
+
+  (testing "converting img elements"
+    (check "<img src='http://example.com/logo.png' />" "![](http://example.com/logo.png)" "We expect img elements to b e converted properly")
+    (check "<img src= \"http://example.com/logo.png\" / >" "![](http://example.com/logo.png)" "We expect img elements to be converted properly")
+    (check "<img src='http://example.com/logo.png'>" "![](http://example.com/logo.png)" "We expect img elements to be converted properly")
+    (check "<img src=http://example.com/logo.png>" "![](http://example.com/logo.png)" "We expect img elements to be converted properly")
+    (check "<img src='http://example.com/logo.png' alt='Example logo' />"
+           "![Example logo](http://example.com/logo.png)"
+           "We expect img elements to be converted properly with alt attrs")
+    (check "<img src='http://example.com/logo.png' alt='Example logo' title='Example title' />"
+           "![Example logo](http://example.com/logo.png \"Example title\")" "We expect img elements to be converted properly with alt and title attrs"))
+
+  (testing "converting anchor elements"
+    (check "<a href='http://example.com/about'>About us</a>"
+           "[About us](http://example.com/about)"
+           "We expect anchor elements to be converted properly")
+    (check "<a href= \"http://www.example.com/about\" title= \"About this company\" >About us</a>"
+           "[About us](http://www.example.com/about \"About this company\")"
+           "We expect an anchor element with a title tag to have correct markdown")
+    (check "<a class= \"some really messy stuff\" href= \"/about\" id= \"donuts3\" title= \"About this company\" >About us</a>"
+           "[About us](/about \"About this company\")"
+           "We expect an anchor element with a title tag to have correct markdown")
+    (check "<a id=\"donuts3\">About us</a>"
+           "<a id=\"donuts3\">About us</a>"
+           "Anchor tags without an href should not be converted"))
+
+  (testing "converting code blocks"
+    (let [html-code
+          (str/join "\n"
+                    ["<pre><code>def foo"
+                     "  # 42 &lt; 9001"
+                     "\t'Hello world!'"
+                     "end</code></pre>"])
+          md-code
+          (str/join "\n"
+                    ["    def foo"
+                     "      # 42 < 9001",
+                     "      'Hello world!'"
+                     "    end"])]
+      (check html-code md-code "We expect code blocks to be converted")))
+
+  (testing "converting list elements"
+    (check "1986. What a great season." "1986\\. What a great season." "We expect numbers that could trigger an ol to be escaped")
+    (check "<ol>\n\t<li>Hello world</li>\n\t<li>Lorem ipsum</li>\n</ol>"
+           (str "1.  Hello world\n"
+                "2.  Lorem ipsum")
+           "We expect ol elements to be converted properly")
+    (check "<ul>\n\t<li>Hello world</li>\n\t<li>Lorem ipsum</li>\n</ul>"
+           (str "*   Hello world\n"
+                "*   Lorem ipsum")
+           "We expect ul elements with line breaks and tabs to be converted properly")
+    (check "<ul class='blargh'><li class='first'>Hello world</li><li>Lorem ipsum</li></ul>"
+           (str "*   Hello world\n"
+                "*   Lorem ipsum")
+           "We expect ul elements with attributes to be converted properly")
+    (check "<ul><li>Hello world</li><li>Lorem ipsum</li></ul><ul><li>Hello world 2</li><li>Lorem ipsum 2</li></ul>"
+           (str "*   Hello world\n"
+                "*   Lorem ipsum\n\n"
+                "*   Hello world 2\n"
+                "*   Lorem ipsum 2")
+           "We expect consecutive ul elements to be converted properly")
+    (check "<ul><li><p>Hello world</p></li><li>Lorem ipsum</li></ul>"
+           (str "*   Hello world\n\n"
+                "*   Lorem ipsum")
+           "We expect li elements with ps to be converted properly")
+    (let [list-with-ps-html
+          (str/join "\n"
+                    ["<ol>"
+                     "  <li>"
+                     "    <p>This is a list item with two paragraphs. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam hendrerit mi posuere lectus.</p>"
+                     "    <p>Vestibulum enim wisi, viverra nec, fringilla in, laoreet vitae, risus. Donec sit amet nisl. Aliquam semper ipsum sit amet velit.</p>"
+                     "  </li>"
+                     "  <li>"
+                     "    <p>Suspendisse id sem consectetuer libero luctus adipiscing.</p>"
+                     "  </li>"
+                     "</ol>"])
+          list-with-ps-md
+          (str/join "\n"
+                    ["1.  This is a list item with two paragraphs. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam hendrerit mi posuere lectus."
+                     ""
+                     "    Vestibulum enim wisi, viverra nec, fringilla in, laoreet vitae, risus. Donec sit amet nisl. Aliquam semper ipsum sit amet velit."
+                     ""
+                     "2.  Suspendisse id sem consectetuer libero luctus adipiscing."])]
+      (check list-with-ps-html list-with-ps-md "We expect lists with paragraphs to be converted"))
+
+    (let [nested-list-html
+          (str/join "\n"
+                    ["<ul>"
+                     "  <li>This is a list item at root level</li>"
+                     "  <li>This is another item at root level</li>"
+                     "  <li>"
+                     "    <ul>"
+                     "      <li>This is a nested list item</li>"
+                     "      <li>This is another nested list item</li>"
+                     "      <li>"
+                     "        <ul>"
+                     "          <li>This is a deeply nested list item</li>"
+                     "          <li>This is another deeply nested list item</li>"
+                     "          <li>This is a third deeply nested list item</li>"
+                     "        </ul>"
+                     "      </li>"
+                     "    </ul>"
+                     "  </li>"
+                     "  <li>This is a third item at root level</li>"
+                     "</ul>"])
+          nested-list-md
+          (str/join "\n"
+                    ["*   This is a list item at root level"
+                     "*   This is another item at root level"
+                     "*   *   This is a nested list item"
+                     "    *   This is another nested list item"
+                     "    *   *   This is a deeply nested list item"
+                     "        *   This is another deeply nested list item"
+                     "        *   This is a third deeply nested list item"
+                     "*   This is a third item at root level"])]
+      (check nested-list-html nested-list-md "We expect nested lists to be converted properly"))
+    )
+
+  )
